ea8fc90fb0e50ff550a751c24911ad812ce8d31e
nexorqk <pkms2a10@gmail.com>
2024-09-24 22:13:15 +0300

    feat: add working solution

--- src/index.js
+++ src/index.js
@@ -1,46 +1,82 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0",
 };
 
 function decode(expr) {
-    // write your solution here
+  const arrWithSlices = [];
+
+  for (let i = 0; i < expr.length / 10; i++) {
+    arrWithSlices.push(expr.slice(i * 10, (i + 1) * 10));
+  }
+
+  const arrWithDividedByTwo = [];
+
+  arrWithSlices.forEach((item) => {
+    const itemArr = [];
+
+    for (let i = 0; i < item.length; i += 2) {
+      itemArr.push(item.slice(i, i + 2));
+    }
+
+    arrWithDividedByTwo.push(itemArr);
+  });
+
+  const encryptedArr = arrWithDividedByTwo.map((item) =>
+    item.map((item) => {
+      if (item === "10") {
+        return ".";
+      }
+      if (item === "11") {
+        return "-";
+      }
+      if (item === "**") {
+        return " ";
+      }
+      return "";
+    })
+  );
+
+  return encryptedArr
+    .map((item) => item.join(""))
+    .map((item) => (MORSE_TABLE[item] ? MORSE_TABLE[item] : " "))
+    .join("");
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
